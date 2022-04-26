ad98a0646d724e358401551baa5048c68583f938
Koshi Gunasinghe <94885967+KoshiSG4@users.noreply.github.com>
2022-04-26 07:24:24 +0530

    Update tests.py

--- Frontend/Project/src/app/tests.py
+++ Frontend/Project/src/app/tests.py
@@ -36,7 +36,7 @@ class TestRestApi(unittest.TestCase):
 class TestBasic(unittest.TestCase):
     def setUp(self):
         # Load test data
-        self.app = app(database='data.json')
+        self.app = app(_Database='data.json')
 
     def test_customer_count(self):
         self.assertEqual(len(self.app.temp), 10)
@@ -50,7 +50,7 @@ class TestBasic(unittest.TestCase):
 class TestComplexData(unittest.TestCase):
     def setUp(self):
         # load test data
-        self.app = app(database='data.json')
+        self.app = app(_Database='data.json')
 
     def test_customer_count(self):
         self.assertEqual(len(self.app.customers), 20)
