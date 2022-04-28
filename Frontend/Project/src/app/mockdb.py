0a38ca5ebe6238d5b06373cc43b9f52967c31ab6
Koshi Gunasinghe <94885967+KoshiSG4@users.noreply.github.com>
2022-04-28 22:57:52 +0530

    Update mockdb.py

--- Frontend/Project/src/app/mockdb.py
+++ Frontend/Project/src/app/mockdb.py
@@ -5,7 +5,7 @@ from mock import patch
 from app import app
 
 MYSQL_USER = "root"
-MYSQL_PASSWORD = ""
+MYSQL_PASSWORD = "Koshila@4"
 MYSQL_DB = "testdb"
 MYSQL_HOST = "localhost"
 MYSQL_PORT = "3306"
