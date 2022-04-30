1c65b880a6542ecd93239ff4cdd0dd4f86cf95da
Koshi Gunasinghe <94885967+KoshiSG4@users.noreply.github.com>
2022-04-30 10:24:46 +0530

    Update app.py

--- Frontend/Project/src/app/app.py
+++ Frontend/Project/src/app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
          
 app.secret_key = "caircocoders-ednalan"
          
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'Koshila@4'
 app.config['MYSQL_DB'] = 'testdb'#----add the name of the data base here -  Biztech 7-----#
