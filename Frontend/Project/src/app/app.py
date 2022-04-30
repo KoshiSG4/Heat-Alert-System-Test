7a783c4396b8cfc927b2872d643c2fdd5c65527b
Koshi Gunasinghe <94885967+KoshiSG4@users.noreply.github.com>
2022-04-30 10:42:46 +0530

    Update app.py

--- Frontend/Project/src/app/app.py
+++ Frontend/Project/src/app/app.py
@@ -8,7 +8,7 @@ app.secret_key = "caircocoders-ednalan"
 app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'Koshila@4'
-app.config['MYSQL_DB'] = 'testdb'#----add the name of the data base here -  Biztech 7-----#
+app.config['MYSQL_DB'] = 'sdgp'#----add the name of the data base here -  Biztech 7-----#
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(app)
  
